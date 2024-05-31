140400f3decef3c8a8632e7f74fd7e67bc43ab9b
mateuszsalata <mateusz.salata@uczen.zsk.poznan.pl>
2024-05-31 04:58:17 +0200

    aktualizacja prismy i backendu 1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//import React from 'react';
+import { BrowserRouter as Router, /*Route, Switch*/ } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+// import Home from './pages/Home';
+// import Pojazdy from './pages/Pojazdy';
+// import Przeglady from './pages/Przeglady';
+// import Wyjazdy from './pages/Wyjazdy';
+// import OProjekcie from 'pages/OProjekcie';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <Router>
+        <div className="App">
+          <Navbar />
+            <div className="content">
+                {/*<Switch>*/}
+                {/*  <Route exact path="/" component={Home} />*/}
+                {/*  <Route path="/pojazdy" component={Pojazdy} />*/}
+                {/*  <Route path="/przeglady" component={Przeglady} />*/}
+                {/*  <Route path="/wyjazdy" component={Wyjazdy} />*/}
+                {/*  <Route path="/o-projekcie" component={OProjekcie} />*/}
+                {/*</Switch>*/}
+            </div>
+
+            <Footer />
+        </div>
+      </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,31 @@
+// Navbar.tsx
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+const Navbar: React.FC = () => {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Strona główna</Link>
+                </li>
+                <li>
+                    <Link to="/pojazdy">Pojazdy i wyposażenie</Link>
+                </li>
+                <li>
+                    <Link to="/przeglady">Przeglądy i naprawy</Link>
+                </li>
+                <li>
+                    <Link to="/wyjazdy">Interwencje i dyspozycje</Link>
+                </li>
+                <li>
+                    <Link to="/o-projekcie">O projekcie</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
