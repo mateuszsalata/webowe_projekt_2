0bc43087e4ae08665a5609ec06620a5df585b17a
Adrian Piechowiak <apiechowiak6572@gmail.com>
2024-05-31 21:50:32 +0200

    switcherv2

--- frontend/src/components/Darkmode.tsx
+++ frontend/src/components/Darkmode.tsx
@@ -1,18 +1,14 @@
 import React, { useState } from 'react';
 
-// Definiowanie typu dla motywu
 type Theme = 'light' | 'dark';
 
-// Komponent przycisku do zmiany motywu
 const ThemeToggleButton: React.FC = () => {
     const [theme, setTheme] = useState<Theme>('light');
 
-    // Funkcja do zmiany motywu
     const toggleTheme = () => {
         setTheme((prevTheme) => prevTheme === 'light' ? 'dark' : 'light');
     };
 
-    // Ustawienie klasy CSS dla motywu
     document.body.className = theme;
 
     return (
